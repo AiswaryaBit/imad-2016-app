db75775f4696ed7746031992cda6c58da814cba4
AiswaryaBit <aiswarya.cs14@bitsathy.ac.in>
2016-10-01 21:21:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,1 @@
-function validateform(){
-var passw=  /^[A-Za-z]\w{7,14}$/;
-var name=document.myform.name.value;
-var pass=document.myform.password.value;
-if(name===null || name===""){
-alert("Name can't be blank");
-return false;
-}else if(pass===null || pass===""){
-alert("password cannnot be enmpty");
-return false;
-}
-else if(pass.length<6 && !pass.match(passw)){
-alert("password cannot be less than 6 chars");
-return false;
-}
-}
 
