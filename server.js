80ffa8d37adae51ce6e37bb70a81d19989723dca
AiswaryaBit <aiswarya.cs14@bitsathy.ac.in>
2016-10-01 21:28:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-  alert('hi');
+ 
 });
 
 app.get('/ui/style.css', function (req, res) {
